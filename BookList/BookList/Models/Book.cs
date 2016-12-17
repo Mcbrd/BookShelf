22c29a5be64dc6c12f40b1efa2d723bc07f09c81
Brad <bradleymcmanus@hotmal.com>
2016-12-17 13:35:46 -0600

    init

---
+++ BookList/BookList/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookList.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public long ISBN13 { get; set; }
+        public int ISBN10 { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+
+    }
+}
